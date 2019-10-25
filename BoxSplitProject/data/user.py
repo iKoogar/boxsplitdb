b3f3cbdfaec073c889b12356cbd654b94781ef13
iKoogar <koo_z@bellsouth.net>
2019-10-25 12:10:42 -0400

    trying stuff

--- BoxSplitProject/data/user.py
+++ BoxSplitProject/data/user.py
@@ -2,7 +2,7 @@ import mongoengine
 
 
 class User(mongoengine.Document):
-	user_id = mongoengine.ObjectIdField(required = True)
+	user_id = mongoengine.ObjectIdField()
 	name = mongoengine.StringField(required = True)
 	email = mongoengine.StringField(required = True)
 
