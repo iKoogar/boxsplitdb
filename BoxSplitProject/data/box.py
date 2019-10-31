f46d80c85ee0cc1ab0832a10daa35c31b8164da0
kooz2@rpi.edu <kooz2@rpi.edu>
2019-10-31 14:59:35 -0400

    changed embedded object ids to reference fields
    trying find_account_by_email

--- BoxSplitProject/data/box.py
+++ BoxSplitProject/data/box.py
@@ -1,19 +1,19 @@
 import mongoengine
 import datetime
 from data.split import Split
+from data.user import User
 
 
 class Box(mongoengine.Document):
-	box_id = mongoengine.ObjectIdField()
-	leader = ReferenceField(User, required = True)
-	name = mongoengine.StringField(required = True)
-	description = mongoengine.StringField(required = True)
-	state = mongoengine.IntField(default = 0)
-	creation_date = mongoengine.DateTimeField(default = datetime.datetime.now)
-	splits = mongoengine.EmbeddedDocumentListField(Split)
+    box_id = mongoengine.ObjectIdField()
+    leader = mongoengine.ReferenceField(User, required=True)
+    name = mongoengine.StringField(required=True)
+    description = mongoengine.StringField(required=True)
+    state = mongoengine.IntField(default=0)
+    creation_date = mongoengine.DateTimeField(default=datetime.datetime.now)
+    splits = mongoengine.EmbeddedDocumentListField(Split)
 
-	meta = {
-		'db_alias': 'core',
-		'collection': 'boxes'
-	}
-
+    meta = {
+        'db_alias': 'core',
+        'collection': 'boxes'
+    }
