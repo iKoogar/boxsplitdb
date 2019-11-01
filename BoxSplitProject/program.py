1412036ace19ed97dabbc0e87a2ef9a9b79c01de
kooz2@rpi.edu <kooz2@rpi.edu>
2019-11-01 15:37:04 -0400

    refactored objects, removing circular dependencies

--- BoxSplitProject/program.py
+++ BoxSplitProject/program.py
@@ -7,10 +7,8 @@ def main():
     mongo_setup.global_init()
 
     split = svc.find_split_by_id("5dbc8670e3e74473175d283e")
-    box = svc.find_box_by_id("5dbc8670e3e74473175d283e")
+    box = svc.find_box_by_id("5dbc8670e3e74473175d283f")
     user = svc.find_user_by_email("poog@split.box")
-
-    print(box.name)
 
     user.boxes.append(box)
     box.splits.append(split)
