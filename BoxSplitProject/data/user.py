c024f2a2bb7d290087a87613ccc9ab6e279a1d5e
kooz2@rpi.edu <kooz2@rpi.edu>
2019-11-01 10:35:45 -0400

    trying to import stuff into user

--- BoxSplitProject/data/user.py
+++ BoxSplitProject/data/user.py
@@ -1,12 +1,12 @@
 import mongoengine
-from data.box import Box
+from BoxSplitProject.data.box import Box
 
 
 class User(mongoengine.Document):
 	user_id = mongoengine.ObjectIdField()
 	name = mongoengine.StringField(required = True)
 	email = mongoengine.StringField(required = True)
-#	own_boxes = mongoengine.ListField(mongoengine.ReferenceField(Box))
+	own_boxes = mongoengine.ListField(mongoengine.ReferenceField(Box))
 
 	# social media stuff
 
